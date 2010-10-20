86d11cc4b1e0ef62eb707d7de26c83afb7c4d7af
Chris Eppstein <chris@eppsteins.net>
2010-10-20 16:43:22 -0700

    In ruby 1.9.2, method arrays are symbols -- not strings

--- lib/active_scaffold/config/sortable.rb
+++ lib/active_scaffold/config/sortable.rb
@@ -3,7 +3,7 @@ module ActiveScaffold::Config
     def initialize(core_config)
       @options = {}
       @core = core_config
-      self.column = core_config.model.new.position_column if @core.model.instance_methods.include? 'acts_as_list_class'
+      self.column = core_config.model.new.position_column if @core.model.instance_methods.map(&:to_s).include? 'acts_as_list_class'
     end
 
     self.crud_type = :update
